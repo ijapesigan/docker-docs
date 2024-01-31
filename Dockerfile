5ebb9e9a5f8d7ab821823997fd4b968a3a28580f
ijapesigan <ijapesigan@gmail.com>
2024-01-31 02:57:22 +0000

    Automated build.

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,10 @@ ENV DEFAULT_USER=rstudio
 ENV PANDOC_VERSION=default
 ENV QUARTO_VERSION=default
 
+RUN /rocker_scripts/install_rstudio.sh latest
+RUN /rocker_scripts/install_pandoc.sh
+RUN /rocker_scripts/install_quarto.sh
+
 # custom
 
 ADD scripts /usr/src/local/src
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-wget https://raw.githubusercontent.com/rocker-org/rocker-versioned2/master/scripts/install_rstudio.sh
-bash install_rstudio.sh
-rm -rf install_rstudio.sh
-wget https://raw.githubusercontent.com/rocker-org/rocker-versioned2/master/scripts/install_pandoc.sh
-bash install_pandoc.sh
-rm -rf install_pandoc.sh
-wget https://raw.githubusercontent.com/rocker-org/rocker-versioned2/master/scripts/install_quarto.sh
-bash install_quarto.sh
-rm -rf install_quarto.sh
-
 bash rproject.sh
 bash tinytex.sh
 bash custom.sh
